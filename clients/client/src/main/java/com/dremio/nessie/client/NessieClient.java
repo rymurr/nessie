5011de565c9500c6b02601de17bac936c94d605a
Robert Stupp <snazy@snazy.de>
2020-12-22 10:26:51 +0100

    De-duplicate http-client-config

--- clients/client/src/main/java/com/dremio/nessie/client/NessieClient.java
+++ clients/client/src/main/java/com/dremio/nessie/client/NessieClient.java
@@ -15,6 +15,12 @@
  */
 package com.dremio.nessie.client;
 
+import static com.dremio.nessie.client.NessieConfigConstants.CONF_NESSIE_AUTH_TYPE;
+import static com.dremio.nessie.client.NessieConfigConstants.CONF_NESSIE_PASSWORD;
+import static com.dremio.nessie.client.NessieConfigConstants.CONF_NESSIE_URL;
+import static com.dremio.nessie.client.NessieConfigConstants.CONF_NESSIE_USERNAME;
+import static com.dremio.nessie.client.NessieConfigConstants.NESSIE_AUTH_TYPE_DEFAULT;
+
 import java.io.Closeable;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
@@ -37,12 +43,6 @@ import com.dremio.nessie.error.NessieNotFoundException;
 
 public class NessieClient implements Closeable {
 
-  public static final String CONF_NESSIE_URL = "nessie.url";
-  public static final String CONF_NESSIE_USERNAME = "nessie.username";
-  public static final String CONF_NESSIE_PASSWORD = "nessie.password";
-  public static final String CONF_NESSIE_AUTH_TYPE = "nessie.auth_type";
-  public static final String NESSIE_AUTH_TYPE_DEFAULT = "BASIC";
-  public static final String CONF_NESSIE_REF = "nessie.ref";
   private final HttpClient client;
 
   public enum AuthType {
